5ef2ad9ccbdb079f815844bf4bc0b0332642fc85
John Sylvain <john.n.sylvain@gmail.com>
2017-07-01 14:04:03 -0400

    redirect route

--- server/routes/app.js
+++ server/routes/app.js
@@ -1,16 +1,24 @@
 import User from '../models/user';
+import Link from '../models/link';
 
 export default function appRoutes(app) {
-  app.get('/setup', (req, res) => {
-    const newUser = new User({
-      username: 'admin',
-      password: 'password',
-      admin: true
-    })
+  // app.get('/setup', (req, res) => {
+  //   const newUser = new User({
+  //     username: 'admin',
+  //     password: 'password',
+  //     admin: true
+  //   })
+  //
+  //   newUser.save((err, user) => {
+  //     if (err) return res.send(err);
+  //     res.json(user);
+  //   })
+  // });
 
-    newUser.save((err, user) => {
-      if (err) return res.send(err);
-      res.json(user);
+  app.get('/:code', (req, res) => {
+    const { code } = req.params;
+    Link.findOne({ code }, (err, link) => {
+      res.redirect(link.url)
     })
   })
 }
